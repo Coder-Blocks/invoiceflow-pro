4f5668e127ab925180206b5b2c34f367a36616dd
Coder-Blocks <ravigarimella9@gmail.com>
2026-04-16 19:28:58 +0530

    Add analytics page in correct location

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**.supabase.co',
-            },
-            {
-                protocol: 'https',
-                hostname: '**.supabase.in',
-            },
-        ],
-    },
-    serverExternalPackages: ['@prisma/client', 'bcryptjs'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.in',
+      },
+    ],
+  },
+  serverExternalPackages: [
+    '@prisma/client',
+    'bcryptjs',
+    'resend',
+    'razorpay',
+    'stripe',
+  ],
 };
 
 export default nextConfig;
